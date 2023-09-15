7351d6fc498d8e909ebc17c93ea405ad29e3b3d0
Han <zh950713@gmail.com>
2023-09-15 14:43:04 +0000

    fix loading concat dim

--- minllama/actions/load.py
+++ minllama/actions/load.py
@@ -1,17 +1,38 @@
 import torch
 
+from tqdm import tqdm
 from pathlib import Path
+from functools import reduce
+
+
+def _get_concat_dim(key, model, weight):
+    weight_shape = weight[key].shape
+    model_shape = reduce(getattr, key.split('.'), model).shape
+    for dim in range(len(model_shape)):
+        if model_shape[dim] != weight_shape[dim]:
+            return dim
+
+
+def _unshard(model, weights, key):
+    concat_dim = _get_concat_dim(key, model, weights[0])
+    if concat_dim is None:
+        return weights[0][key]
+    else:
+        return torch.concat([
+            weight[key] for weight in weights
+        ], dim=concat_dim)
 
 
 def _load_sharded_checkpoints(model, checkpoints, dtype):
-    weights = [torch.load(ckpt) for ckpt in checkpoints]
+    weights = [torch.load(ckpt, map_location='cpu') for ckpt in checkpoints]
     # NOTE: we will recycle the memories by deleting the used keys
     keys = list(weights[0].keys())
-    for key in keys:
+    for key in tqdm(keys):
+        # Dirty fix for unused Llama parameters
+        if 'rope.freqs' == key:
+            continue
         # Unshard the weights
-        single_param_state_dict = {key: torch.concat([
-            weight[key] for weight in weights
-        ]).to(dtype)}
+        single_param_state_dict = {key: _unshard(model, weights, key)}
         # Load by assignment
         model.load_state_dict(
             single_param_state_dict, assign=True, strict=False)
@@ -22,10 +43,7 @@ def _load_sharded_checkpoints(model, checkpoints, dtype):
 
 def load_checkpoint(model, path, dtype=torch.bfloat16):
     path = Path(path)
-    # model = model.to_empty(device='cpu')  # Lazy memory allocation
-
-    sharded_ckpt = Path(path).is_dir()
-    if sharded_ckpt:
+    if path.is_dir():
         # Original llama weights
         num_ckpts = len(list(path.glob('consolidated.*.pth')))
         sorted_ckpts = [
@@ -34,6 +52,6 @@ def load_checkpoint(model, path, dtype=torch.bfloat16):
         _load_sharded_checkpoints(model, sorted_ckpts, dtype)
     else:
         # Full state dict in a single file
-        model.load_state_dict(torch.load(path))
+        model.load_state_dict(torch.load(path, map_location='cpu'))
 
     return model
